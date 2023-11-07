9d167a0a65366ab4424045f77f6041b33480ead1
Matheus Sousa <dsousamt@gmail.com>
2023-11-07 14:09:23 -0300

    Cria variáveis de contexto, models e service

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,8 +2,7 @@ function Navbar() {
   return (
     <>
       <nav className="
-        w-full bg-indigo-900 text-white flex justify-center py-4
-      ">
+        w-full bg-indigo-900 text-white flex justify-center py-4">
         <div className="container flex justify-between text-lg">
           Blog do Matheus
           <div className="flex gap-4">
