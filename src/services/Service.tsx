fabdd14243951a342d07e5b41c88b1d8884e0591
Matheus Sousa <dsousamt@gmail.com>
2023-11-22 13:41:58 -0300

    Revert "Ajustes para deploy"
    This reverts commit d7ad2075ff6d0000d9df52099315cf6a9372a4af.

--- src/services/Service.tsx
+++ src/services/Service.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: 'https://blogdomatheus.onrender.com'
 })
 
 export const login = async (url: string, dados: Object, setDados: Function) => {
